14c3e607e1dc577b149ee975c0938e246bc37b18
Anant Kumar <anantk29082001@gmail.com>
2020-03-17 18:56:33 +0530

    Started with sqlite

---
+++ TimeTable/app/src/main/java/me/eijaz/tabstutorial/AddNoteActivity.kt
@@ -0,0 +1,12 @@
+package me.eijaz.tabstutorial
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
